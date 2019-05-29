d28ad25af63d45d57bae9e4839bae884f107ce49
Daniel Kluss <codepilot@gmail.com>
2019-05-29 03:08:16 -0700

    improved physics speed with less dom reads

--- index.js
+++ index.js
@@ -2,12 +2,13 @@
 
 const pong = document.querySelector('.pong');
 const allCircles = document.querySelectorAll('.pong circle');
+const fixed = document.querySelectorAll('.fixed>circle');
 const mobile = document.querySelectorAll('.mobile>circle');
 
 function differenceVector(a, b) {
   return {
-    x: (b.cx.baseVal.valueInSpecifiedUnits - a.cx.baseVal.valueInSpecifiedUnits),
-    y: (b.cy.baseVal.valueInSpecifiedUnits - a.cy.baseVal.valueInSpecifiedUnits),
+    x: (b.x - a.x),
+    y: (b.y - a.y),
   };
 }
 
@@ -24,28 +25,39 @@ function forceVector(a, b) {
   const vec = differenceVector(a, b);
   const dist = distance(a, b);
   const normVec = {x: vec.x / dist, y: vec.y / dist};
-  const scale = -1 / (dist ** 2) * 2000;
+  const scale = -1 / (dist ** 2) * 10;
   return {
     x: normVec.x * scale,
     y: normVec.y * scale,
   };
 }
 
+const fixedLocations = [...fixed].map(f=> ({x: f.cx.baseVal.valueInSpecifiedUnits, y: f.cy.baseVal.valueInSpecifiedUnits}));
+
 function physics() {
+  // let totalEnergy = 0;
   for(let m of mobile) {
-    const sv = [...allCircles].filter(f=>f!==m).map((f)=>forceVector(m, f)).reduce((p, c)=> ({x: p.x + c.x, y: p.y + c.y}), {x: 0, y: 0});
+    const mp = {x: m.cx.baseVal.valueInSpecifiedUnits, y: m.cy.baseVal.valueInSpecifiedUnits};
+    const fv = fixedLocations.map((fp)=>forceVector(mp, fp)).reduce((p, c)=> ({x: p.x + c.x, y: p.y + c.y}), {x: 0, y: 0});
+    const sv = [...mobile].filter(f=>f!==m).map((f)=>forceVector(mp, {x: f.cx.baseVal.valueInSpecifiedUnits, y: f.cy.baseVal.valueInSpecifiedUnits})).reduce((p, c)=> ({x: p.x + c.x, y: p.y + c.y}), fv);
+    // console.log({mp, fv, sv});
     const delta = {x: sv.x + parseFloat(m.dataset.dx), y: sv.y + parseFloat(m.dataset.dy)}
     m.dataset.dx = delta.x;
     m.dataset.dy = delta.y;
     m.cx.baseVal.valueInSpecifiedUnits += delta.x;
     m.cy.baseVal.valueInSpecifiedUnits += delta.y;
+    // totalEnergy += Math.sqrt(delta.x ** 2 + delta.y ** 2);
   }
+  // console.log(totalEnergy);
 }
 
 function render() {
-  for(let i = 0; i < 1; i++) {
+  // const startTime = performance.now();
+  for(let i = 0; i < 40; i++) {
     physics();
   }
+  // const finishTime = performance.now();
+  // console.log(finishTime - startTime);
   requestAnimationFrame(render);
 }
 
